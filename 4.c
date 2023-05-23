ed651c4c957cb01adcff8d95375b300111a92576
NahuelLV <nahuellinaresviau@gmail.com>
2023-05-23 19:12:06 -0300

    actualizoreadme

---
+++ 4.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int factorial(int n) {
+
+
+   int r = 1;
+   int i = 1;
+
+while (i <=n) {
+
+        r *=i;
+        i++;
+
+}
+   return r;
+
+}
+
+int main(int argc, char *argv[]){
+
+
+        int a = atoi(argv[1]);                        
+        if (a <= 0) {
+        printf("Error\n");
+        return 0;                                             }
+        printf("Factorial de %d=%d\n", a, factorial(a));
+        return 0;
+}
--- README.md
+++ README.md
@@ -15,9 +15,7 @@
 
 ## Loops
 
--[Ejercicio 4-1](/vim 4-1.c 4-1.c)
-
-
+-[Ejercicio 4-1](/vim4-1.c 4-1.c)
 
 
 
