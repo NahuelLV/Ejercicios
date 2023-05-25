6a46eb2559e015350d87f1429138c7d30422f361
NahuelLV <nahuellinaresviau@gmail.com>
2023-05-25 18:18:02 -0300

    agrego los ejwrcicios 19H y 20H

---
+++ 19H.c
@@ -0,0 +1,13 @@
+
+
+#include<stdio.h>
+
+int main(int argc, char *argv[1]) {
+
+
+	printf("Hola %s, %s y %s\n", argv[3], argv[2], argv[1]);
+	return 0;
+
+
+
+}
---
+++ 20H.c
@@ -0,0 +1,59 @@
+
+
+#include<stdio.h>
+
+int main(int main, char *argv[]) {
+
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	printf("Hola mundo \n");
+	return 0;
+}
