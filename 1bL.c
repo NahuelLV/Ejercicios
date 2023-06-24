7ca23f4392077fbc73f5a7d50feab765e2032aa1
NahuelLV <nahuellinaresviau@gmail.com>
2023-06-24 12:55:24 -0300

    agrego ejercicios

---
+++ 1bL.c
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc, char *argv[]) {
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);    
+	int c = atoi(argv[3]);
+
+	if (a > b && a > c) {
+		printf("%d", a);
+
+	}else if(b > a && c < b){
+
+		printf("%d", b);
+							          }else if (c > a && b < c){
+									  printf("%d", c);
+								  }
+	printf("\n");
+	return 0;
+}
